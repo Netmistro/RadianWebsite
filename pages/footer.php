997b1eb4cb89b8663e908c21ecf55fa4ea6c88c5
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2018-10-11 15:15:48 -0400

    Update - 2018-10-11
    Fixes to Header, Footer

--- pages/footer.php
+++ pages/footer.php
@@ -11,16 +11,56 @@
 <head>
     <meta charset="UTF-8">
     <title>RADIAN H.A. Limited</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
 </head>
 <body>
-<div class="content-wrap">
-    <section class="footer-wrap"
-    <p>The leading scaffolding training provider in Trinidad & Tobago
-        and the Caribbean. Designed & Maintained by Radian H.A. Limited.</p>
-    <p><a href="mailto:arnold.bradshaw@rhatt.com">Webmaster</a>
-        <a href="http://www.facebook.com"> Facebook </a></p>
-    <p>Copyright &copy; 2015 Copyright Radian HA Ltd.</p>
-    </section>
-</div>
+<!------ Footer ---------->
+<footer class="section footer-classic context-dark bg-image" style="background: #2d3246;">
+    <div class="container">
+        <div class="row row-30">
+            <div class="col-md-4 col-xl-5">
+                <h5>About</h5>
+                <div class="pr-xl-4"><a class="brand" href="index.php"></a>
+                    <p>The leading scaffolding training provider in Trinidad & Tobago and the Caribbean.</p>
+                    <!-- Rights-->
+                    <p class="rights"><span>© </span><span class="copyright-year">2018</span>
+                        <span>RADIAN H.A. Limited</span><span>.</span><span>All Rights Reserved.</span>
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <h5>Contacts</h5>
+                <dl class="contact-list">
+                    <dt>Address:</dt>
+                    <dd>#187 Helen Street, Marabella, Trinidad</dd>
+                </dl>
+                <dl class="contact-list">
+                    <dt>email:</dt>
+                    <dd><a href="mailto:#">radianhaltd@gmail.com</a></dd>
+                </dl>
+                <dl class="contact-list">
+                    <dt>TSTT/BMobile:</dt>
+                    <dd><a href="tel:#">+1 868 658-0293</a><span>or</span> <a href="tel:#">+1 868 658-5946</a></dd>
+                    <dt>FLOW:</dt>
+                    <dd><a href="tel:#">+1 868 658-8294</a><span>or</span> <a href="tel:#">+1 868 223-0293</a></dd>
+                    <dt>Digicel:</dt>
+                    <dd><a href="tel:#">+1 868 387-0293</a></dd>
+                </dl>
+
+            </div>
+            <div class="col-md-4 col-xl-3">
+                <h5>Quick Links</h5>
+                <ul class="nav-list">
+                    <li><a href="../pages/about-us.php">About</a></li>
+                    <li><a href="../pages/contact-us.php">Contacts</a></li>
+                    <li><a href="http://www.facebook.com/radianhaltd/">Facebook</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
--- pages/header.php
+++ pages/header.php
@@ -10,19 +10,22 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>RADIAN H.A. Limited</title>
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-<div class="content-wrap">
-    <section class="header">
-        <img src="images/radian-logo.png" alt="Radian" height="150">
-        <a href="index.php">Home</a>
-        <a href="pages/products.php">Products</a>
-        <a href="pages/services.php">Services</a>
-        <a href="pages/contact-us.php">Contact Us</a>
-        <a href="pages/about-us.php">About Us</a>
-        <h1>Welcome to RADIAN H.A. Limited</h1>
-    </section>
+<div class="header">
+    <img src="../images/radian-logo.png" alt="RADIAN">
+    <div class="header-right">
+        <a class="active" href="/index.php">Home</a>
+        <a href="../pages/contact-us.php">Contact</a>
+        <a href="../pages/about-us.php">About</a>
+
+    </div>
+</div>
+<div style="padding-left:20px">
+    <h1>RADIAN H.A. Limited</h1>
+    <p>Scaffolding Materials Sale, Rental, Training, Rope Access Equipment</p>
 </div>
 </body>
 </html>
