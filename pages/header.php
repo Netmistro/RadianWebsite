0a5075ce31b3c4375cab8ad7c7e063e5bf50ea22
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2018-10-12 09:04:36 -0400

    Update - 2018-10-12
    Updated Header Logo alignment and text. Added telephone contactr numbers and align buttons

--- pages/header.php
+++ pages/header.php
@@ -16,10 +16,13 @@
 <body>
 
 <div class="header-row">
-    <div class="header-logo">
+    <div class="radian-logo">
+        <img src="../images/radian-logo.png" width="80">
+    </div>
+    <div class="header-name">
         <h1>RADIAN H.A. Limited</h1>
         <p>187 Helen Street, Marabella, Trinidad</p>
-        <p>TSTT: +1 868 658-0293/HHHH/HHHH</p>
+        <p>TSTT: +1(868) 658-0293/8294/5946</p>
     </div>
     <div class="header-right">
         <a class="active" href="../index.php">Home</a>
@@ -27,7 +30,6 @@
         <a href="../pages/products.php">Products</a>
         <a href="../pages/contact-us.php">Contact</a>
         <a href="../pages/profile.php">About Us</a>
-
     </div>
 </div>
 </body>
