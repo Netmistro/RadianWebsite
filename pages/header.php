efe7a8752e15fa43f3d54a2a4d12fd5b98ee2547
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2018-10-12 15:50:54 -0400

    Update - 2018-10-12
    Modified header.css and profile page

--- pages/header.php
+++ pages/header.php
@@ -12,9 +12,10 @@
         <img src="../images/radian-logo.png" alt="Radian" width="120">
     </div>
     <div class="header-name">
-        <h1>RADIAN H.A. Limited</h1><br>
+        <h1>RADIAN H.A. Limited</h1>
         <p>187 Helen Street, Marabella, Trinidad</p>
         <p>TSTT: +1(868) 658-0293/8294/5946</p>
+        <p>Others: +1(868) 223-8823/8824</p>
     </div>
     <div class="header-right">
         <a class="active" href="../index.php">Home</a>
