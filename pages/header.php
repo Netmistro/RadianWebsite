0938e797e7444a0b293d13ef7ffb5ec203c00b40
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2018-10-26 23:57:32 -0400

    Deleted broken code in initialize.php and fixed all the hyperlinks to each page so they work.

--- pages/header.php
+++ pages/header.php
@@ -26,13 +26,65 @@ include 'initialize.php';
         <p>Others: +1(868) 223-8823/8824</p>
     </div>
     <div class="header-right">
-        <a class="active" href=<?php "../index.php" ?>>Home</a>
 
-        <a href="<?php products($products); ?>">Products</a>
-        <a href="<?php echo 'Services'; ?>">Services</a>
+        <!--  Code for Home Page Hyperlink  -->
+        <?php
+        if (empty(scandir('pages')) == true) {
+            $Home = '../index.php';
+        } else {
+            $Home = 'index.php';
+        }
+        ?>
+        <a class="active" href=<?php echo $Home; ?>>Home</a>
 
-        <a href="<?php echo 'our-clients.php'; ?>">Our Clients</a>
-        <a href="<?php echo 'contact-us.php'; ?>">Contact</a>
-        <a href="<?php echo 'profile.php'; ?>">About Us</a>
+        <!--  Code for Products Hyperlink  -->
+        <?php
+        if (empty(scandir('pages')) == true) {
+            $Products = 'products.php';
+        } else {
+            $Products = 'pages/products.php';
+        }
+        ?>
+        <a href="<?php echo $Products; ?>">Products</a>
+
+        <!--  Code for Services Hyperlink  -->
+        <?php
+        if (empty(scandir('pages')) == true) {
+            $Services = 'services.php';
+        } else {
+            $Services = 'pages/services.php';
+        }
+        ?>
+        <a href="<?php echo $Services; ?>">Services</a>
+
+        <!--  Code for Our Clients Hyperlink  -->
+        <?php
+        if (empty(scandir('pages')) == true) {
+            $OurClients = 'our-clients.php';
+        } else {
+            $OurClients = 'pages/our-clients.php';
+        }
+        ?>
+        <a href="<?php echo $OurClients; ?>">Our Clients</a>
+
+        <!--  Code for Contact Us Hyperlink  -->
+        <?php
+        if (empty(scandir('pages')) == true) {
+            $ContactUs = 'contact-us.php';
+        } else {
+            $ContactUs = 'pages/contact-us.php';
+        }
+        ?>
+        <a href="<?php echo $ContactUs; ?>">Contact</a>
+
+        <!--  Code for Profile Hyperlink  -->
+        <?php
+        if (empty(scandir('pages')) == true) {
+            $Profile = 'profile.php';
+        } else {
+            $Profile = 'pages/profile.php';
+        }
+        ?>
+        <a href="<?php echo $Profile; ?>">About Us</a>
     </div>
 </div>
--- pages/initialize.php
+++ pages/initialize.php
@@ -7,15 +7,3 @@
  */
 // Assign file paths to PHP constants
 // __FILE__ returns the current path of this file
-// dirname() returns the path of the parent directory
-
-//define("PAGES_PATH", dirname(__FILE__));
-
-function products(){
-        if (empty(scandir('pages')) == true) {
-            $link_name = 'products.php';
-        } else {
-            $products = 'pages/products.php';
-        }
-}
-
