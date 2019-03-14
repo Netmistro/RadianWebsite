e2a0862b3a6a4ac75689ad7374d94770543a90be
Arnold Bradshaw <ar17afr@herts.ac.uk>
2019-03-14 15:25:45 -0400

    Update - 2019-03-14
    Update to add the ergodyne line of tool belts

--- tool-belts.php
+++ tool-belts.php
@@ -23,22 +23,115 @@ include('header.php');
     <br>
     <h1>Tool Belts</h1><br>
 
-    <!--    Single Prong Belts - Black -->
+    <!--    Ergodyne 5506S Tool Belt -->
     <div class="tools-container">
         <div class="tools-image">
-            <img src="images/leather/LSB-0010.png" alt="Leather Products" width="300">
+            <img src="images/tool-belts/arsenal-5506s-ergodyne.png" alt="Leather Products" width="300">
         </div>
         <div class="tools-details">
-            <b><span class="product-code">Product Code:</span> LSB-0010</b><br>
-            <span class="tag-label">Description: </span><span>BIG BEN 2" Heavy Duty Belt with Eyelets - Black</span><br>
-            <span class="tag-label">Manufacturer: </span><span>BIG BEN</span><br>
-            <p>Tough Scaffolders belts have elasticated chalk/pen holders built-on. Reinforced eyelet holes for
-                hard wear and longer lasting</p>
+            <b><span class="product-code">Product Code:</span> WPB-0009</b><br>
+            <span class="tag-label">Description: </span><span>ARSENAL® 5506S Installer/Drill Holder Tool Rig</span><br>
+            <span class="tag-label">Manufacturer: </span><span>ERGODYNE</span><br>
+            <p>Installer/Drill Holder Tool Rig with Suspenders: Tool rig for maximum tool capacity and cordless drill
+                accessibility.</p>
+            <h3>Features</h3>
             <ul>
-                <li>LSB 0010 - Size 38"</li>
-                <li>LSB 0008 - Size 60"</li>
-                <li>LSB 0007 - Size 44"</li>
-                <li>LSB 0006 - Size 40"</li>
+                <li>1680D ballistic Polyester</li>
+                <li>5in Padded Base Layer Tool Belt (5555)</li>
+                <li>Reinforced bindings, semi-rigid</li>
+                <li>Ambidextrous drill holder pouch</li>
+                <li>Padded quick-adjust suspenders included (5560)</li>
+                <li>Aerial pouch with safety top (5518)</li>
+                <li>Patented Storm Drains</li>
+                <li>Fits waist sizes 32in - 54in (81cm - 137cm)</li>
+                <li>Available in Grey ONLY</li>
+                <li>Available in L (WPB-0009) & XL (WPB-0010)</li>
+            </ul>
+            <h3>Application</h3>
+            <ul>
+                <li>Electricians</li>
+                <li>Carpentry</li>
+                <li>Technicians</li>
+                <li>Service</li>
+                <li>Contractors</li>
+                <li>HVAC</li>
+                <li>Handyman</li>
+                <li>Trades</li>
+            </ul>
+        </div>
+    </div>
+    <br>
+
+    <!--    Ergodyne 5504 Tool Belt -->
+    <div class="tools-container">
+        <div class="tools-image">
+            <img src="images/tool-belts/arsenal-5504-ergodyne.png" alt="Leather Products" width="300">
+        </div>
+        <div class="tools-details">
+            <b><span class="product-code">Product Code:</span> WPB-0007</b><br>
+            <span class="tag-label">Description: </span><span>ARSENAL® 5504 34-Pocket Tool Rig-Synthetic</span><br>
+            <span class="tag-label">Manufacturer: </span><span>ERGODYNE</span><br>
+            <p>Framer/Builder Tool Rig: Large capacity, 34-pocket poly tool rig for framing and general carpentry. 3 in
+                1 base layer tool belt with vented foam padding</p>
+            <h3>Features</h3>
+            <ul>
+                <li>1680D ballistic Polyester</li>
+                <li>Reinforced bindings, semi-rigid</li>
+                <li>Abrasion-resistant pouch bases</li>
+                <li>Tape holder - large (5571)</li>
+                <li>Nickel-plated hammer holder (5575)</li>
+                <li>Padded Suspenders - sold separately (5560)</li>
+                <li>Fits waist sizes 32in - 54in (81cm - 137cm)</li>
+                <li>Available in Grey ONLY</li>
+                <li>Available in L (WPB-0007) & XL (WPB-0008)</li>
+            </ul>
+            <h3>Application</h3>
+            <ul>
+                <li>Carpentry</li>
+                <li>Fishing</li>
+                <li>Framing</li>
+                <li>Contractors</li>
+                <li>Heavy Construction</li>
+                <li>Light Construction</li>
+                <li>Handyman</li>
+                <li>Trades</li>
+            </ul>
+        </div>
+    </div>
+    <br>
+
+    <!--    Ergodyne 5500 Tool Belt -->
+    <div class="tools-container">
+        <div class="tools-image">
+            <img src="images/tool-belts/arsenal-5500-ergodyne.png" alt="Leather Products" width="300">
+        </div>
+        <div class="tools-details">
+            <b><span class="product-code">Product Code:</span> WPB-0005</b><br>
+            <span class="tag-label">Description: </span><span>ARSENAL® 5500 28-Pocket Tool Rig-Synthetic</span><br>
+            <span class="tag-label">Manufacturer: </span><span>ERGODYNE</span><br>
+            <p>Framer/Builder Tool Rig: Large capacity, 28-pocket synthetic tool rig for Framing and general
+                Carpentry</p>
+            <h3>Features</h3>
+            <ul>
+                <li>1680D ballistic Polyester</li>
+                <li>5in Padded Base Layer Tool Belt (5555)</li>
+                <li>16-Pocket Tool Pouch (5522)</li>
+                <li>11-Pocket Fastener Pouch (5532)</li>
+                <li>Nickel-plated hammer holder (5575)</li>
+                <li>Padded Suspenders - sold separately (5560)</li>
+                <li>Fits waist sizes 32in - 54in (81cm - 137cm)</li>
+                <li>Available in Grey ONLY</li>
+                <li>Available in L (WPB-0005) & XL (WPB-0006)</li>
+            </ul>
+            <h3>Application</h3>
+            <ul>
+                <li>Carpentry</li>
+                <li>Fishing</li>
+                <li>Framing</li>
+                <li>Contractors</li>
+                <li>Construction</li>
+                <li>Handyman</li>
+                <li>Trades</li>
             </ul>
         </div>
     </div>
