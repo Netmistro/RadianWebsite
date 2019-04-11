c2befbabef443ab8aa126f1003de5676441ac658
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2019-04-11 14:53:21 -0400

    Started Photo Gallery Code

--- boss-towers.php
+++ boss-towers.php
@@ -33,9 +33,8 @@ include('header.php');
                 <b><span class="product-code">Product Code:</span> STS4001</b><br>
                 <span class="tag-label">Description: </span><span>BoSS Evolution Ladderspan - Scaffold Tower</span><br>
                 <p>
-                    Generally regarded as the best tower available. The next generation of BoSS scaffold tower,
-                    manufactured using state–of–the–art laser cutting & robotic welding technology. This exceptional
-                    aluminium access tower offers all the features of original BoSS & more.</p><br>
+                    Generally regarded as the best tower available. The next generation of BoSS scaffold tower, manufactured using state–of–the–art laser
+                    cutting & robotic welding technology. This exceptional aluminium access tower offers all the features of original BoSS & more.</p><br>
                 <h3>Specifications</h3>
                 <ul>
                     <li>Available in 450mm and 1.8m</li>
@@ -44,9 +43,7 @@ include('header.php');
                     <li>AGR and 3T (through the trap) build method</li>
                     <li>Frame widths of 850mm & 1450mm</li>
                     <li>3 Deck lengths: 1.8m, 2.5m & 3.2m. However we only stock 1.8m</li>
-                    <li>Safe working load of 275kg per platform level up to a maximum of 950kg per tower (including self
-                        weight)
-                    </li>
+                    <li>Safe working load of 275kg per platform level up to a maximum of 950kg per tower (including self weight)</li>
                     <li>Patented double action trigger operated locking claws</li>
                     <li>Floating spigots for easy assembly & dismantling</li>
                     <li>Self cleaning adjustable legs</li>
@@ -54,8 +51,7 @@ include('header.php');
                     <li>Increased capabilities through a number of BoSS tower additions</li>
                     <li>Whole life support includes manufacturers Kite marked BoSS Repair Service</li>
                     <li>Towers of 8.7m and above should be tied in when used externally</li>
-                    <li>Towers at 8.2m platform height are supplied with large SP15 stabilisers as at this height they can
-                        be used externally without tying in
+                    <li>Towers at 8.2m platform height are supplied with large SP15 stabilisers as at this height they can be used externally without tying in
                     <li>Fully EN1004 compliant</li>
                 </ul>
             </div>
@@ -65,9 +61,7 @@ include('header.php');
     </div>
 </body>
 <footer>
-    <?php
-    include('footer.php');
-    ?>
+    <?php include('footer.php'); ?>
 </footer>
 
 </html>
---
+++ training-gallery.php
@@ -0,0 +1,27 @@
+<?php
+// Created by Arnold Bradshaw
+// 11th April, 2019
+session_start();
+require 'header.php';
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Photo Gallery</title>
+    <meta name="RADIAN H.A. Limited" content="Everything Scaffolding, Sale & Rental of Materials, Tools & Training">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/training-gallery.css">
+</head>
+
+<body>
+
+
+</body>
+<footer>
+    <?php require 'footer.php'; ?>
+</footer>
+
+</html>
