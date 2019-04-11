6a65c5a5955e1e15845d335edd58557d73787603
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2019-04-11 16:00:56 -0400

    Training Gallery Development

--- training-gallery.php
+++ training-gallery.php
@@ -17,7 +17,56 @@ require 'header.php';
 </head>
 
 <body>
+    <section class="gallery-links">
+        <div class="wrapper">
+            <h2>Gallery</h2>
+            <div class="gallery-container">
+                <a href="#">
+                    <div></div>
+                    <h3>This is a title</h3>
+                    <p>This is a paragraph</p>
+                </a>
 
+                <a href="#">
+                    <div></div>
+                    <h3>This is a title</h3>
+                    <p>This is a paragraph</p>
+                </a>
+
+                <a href="#">
+                    <div></div>
+                    <h3>This is a title</h3>
+                    <p>This is a paragraph</p>
+                </a>
+
+                <a href="#">
+                    <div></div>
+                    <h3>This is a title</h3>
+                    <p>This is a paragraph</p>
+                </a>
+
+                <a href="#">
+                    <div></div>
+                    <h3>This is a title</h3>
+                    <p>This is a paragraph</p>
+                </a>
+                <?php
+                if (isset($_SESSION['username'])) {
+                    echo '< div class ="gallery-upload ">
+                    <form action ="./includes/gallery-upload.inc.php" method ="POST" enctype ="multipart/form-data ">
+                     <input type ="text" name ="filename" placeholder ="File Name ... ">
+                     <input type ="text" name ="filetitle" placeholder ="Image Title ... ">
+                     <input type ="text" name ="filedesc" placeholder ="Image Description ... ">
+                     <input type ="file" name ="filename ">
+                     <button type ="submit" name =submit >UPLOA D < /butto n>
+                     < /form>
+                     < /div>';
+                }
+                ?>
+            </div>
+
+        </div>
+    </section>
 
 </body>
 <footer>
