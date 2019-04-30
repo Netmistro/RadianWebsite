1b45a831657c8e65874023f114c29cc48336f9a7
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2019-04-30 09:34:37 -0400

    Deleted lightbox data to start a new system.

--- boss-towers.php
+++ boss-towers.php
@@ -21,44 +21,48 @@ include('header.php');
 </head>
 
 <body>
-    <div class="system-scaff-page">
-        <br>
-        <h1>BoSS Aluminium Towers</h1><br>
-        <!-- Boss Aluminium Towers -->
-        <div class="system-scaff-container">
-            <div class="system-scaff-image">
-                <img src="images/boss-towers/boss-alum-tower.png" alt="Boss Towers" width="300">
-            </div>
-            <div class="system-scaff--details">
-                <b><span class="product-code">Product Code:</span> STS4001</b><br>
-                <span class="tag-label">Description: </span><span>BoSS Evolution Ladderspan - Scaffold Tower</span><br>
-                <p>
-                    Generally regarded as the best tower available. The next generation of BoSS scaffold tower, manufactured using state–of–the–art laser
-                    cutting & robotic welding technology. This exceptional aluminium access tower offers all the features of original BoSS & more.</p><br>
-                <h3>Specifications</h3>
-                <ul>
-                    <li>Available in 450mm and 1.8m</li>
-                    <li>New ergonomic Integral ladder system for increased grip and safety</li>
-                    <li>Frame horizontals have 500mm spacing and ribbed tubing for increased grip</li>
-                    <li>AGR and 3T (through the trap) build method</li>
-                    <li>Frame widths of 850mm & 1450mm</li>
-                    <li>3 Deck lengths: 1.8m, 2.5m & 3.2m. However we only stock 1.8m</li>
-                    <li>Safe working load of 275kg per platform level up to a maximum of 950kg per tower (including self weight)</li>
-                    <li>Patented double action trigger operated locking claws</li>
-                    <li>Floating spigots for easy assembly & dismantling</li>
-                    <li>Self cleaning adjustable legs</li>
-                    <li>Deck board edge protection and easy to use toeboard system</li>
-                    <li>Increased capabilities through a number of BoSS tower additions</li>
-                    <li>Whole life support includes manufacturers Kite marked BoSS Repair Service</li>
-                    <li>Towers of 8.7m and above should be tied in when used externally</li>
-                    <li>Towers at 8.2m platform height are supplied with large SP15 stabilisers as at this height they can be used externally without tying in
-                    <li>Fully EN1004 compliant</li>
-                </ul>
-            </div>
+<div class="system-scaff-page">
+    <br>
+    <h1>BoSS Aluminium Towers</h1><br>
+    <!-- Boss Aluminium Towers -->
+    <div class="system-scaff-container">
+        <div class="system-scaff-image">
+            <img src="images/boss-towers/boss-alum-tower.png" alt="Boss Towers" width="300">
         </div>
-        <br>
-
+        <div class="system-scaff--details">
+            <b><span class="product-code">Product Code:</span> STS4001</b><br>
+            <span class="tag-label">Description: </span><span>BoSS Evolution Ladderspan - Scaffold Tower</span><br>
+            <p>
+                Generally regarded as the best tower available. The next generation of BoSS scaffold tower, manufactured
+                using state–of–the–art laser cutting & robotic welding technology. This exceptional aluminium access
+                tower offers all the features of original BoSS & more.</p><br>
+            <h3>Specifications</h3>
+            <ul>
+                <li>Available in 450mm and 1.8m</li>
+                <li>New ergonomic Integral ladder system for increased grip and safety</li>
+                <li>Frame horizontals have 500mm spacing and ribbed tubing for increased grip</li>
+                <li>AGR and 3T (through the trap) build method</li>
+                <li>Frame widths of 850mm & 1450mm</li>
+                <li>3 Deck lengths: 1.8m, 2.5m & 3.2m. However we only stock 1.8m</li>
+                <li>Safe working load of 275kg per platform level up to a maximum of 950kg per tower (including self
+                    weight)
+                </li>
+                <li>Patented double action trigger operated locking claws</li>
+                <li>Floating spigots for easy assembly & dismantling</li>
+                <li>Self cleaning adjustable legs</li>
+                <li>Deck board edge protection and easy to use toeboard system</li>
+                <li>Increased capabilities through a number of BoSS tower additions</li>
+                <li>Whole life support includes manufacturers Kite marked BoSS Repair Service</li>
+                <li>Towers of 8.7m and above should be tied in when used externally</li>
+                <li>Towers at 8.2m platform height are supplied with large SP15 stabilisers as at this height they can
+                    be used externally without tying in
+                <li>Fully EN1004 compliant</li>
+            </ul>
+        </div>
     </div>
+    <br>
+
+</div>
 </body>
 <footer>
     <?php include('footer.php'); ?>
--- index.php
+++ index.php
@@ -20,63 +20,65 @@ require 'header.php';
 </head>
 
 <body>
-    <div class="main-page">
+<div class="main-page">
 
-        <div class="content-wrap">
-            <br>
-            <img src="./images/radian-logo.png" alt="RADIAN HA Limited" width="150">
-            <h2>The leading Scaffold Training provider in Trinidad & Tobago and the Caribbean</h2>
-            <p>Radian H.A. Ltd. is a dynamic, customer-driven business established in 2001; we primarily engage in the provision of comprehensive training programs in Scaffolding
-                & Form Work. We work in conjunction with Safety & Access Ltd.(UK) the leading scaffolding & formwork training agency in the United Kingdom. We also provide the widest
-                range of scaffolding, Scaffolding Tools, Scaffolding Accessories and Safety Equipment in Trinidad & Tobago for both hire and sale. Radian H.A. Ltd also has a truly
-                unique market position as we never undertake any contract work, which we would consider to be direct competition with on-site contractors, many of whom are our
-                customers.</p>
-            <br><br>
+    <div class="content-wrap">
+        <br>
+        <img src="./images/radian-logo.png" alt="RADIAN HA Limited" width="150">
+        <h2>The leading Scaffold Training provider in Trinidad & Tobago and the Caribbean</h2>
+        <p>Radian H.A. Ltd. is a dynamic, customer-driven business established in 2001; we primarily engage in the
+            provision of comprehensive training programs in Scaffolding & Form Work. We work in conjunction with Safety
+            & Access Ltd.(UK) the leading scaffolding & formwork training agency in the United Kingdom. We also provide
+            the widest range of scaffolding, Scaffolding Tools, Scaffolding Accessories and Safety Equipment in Trinidad
+            & Tobago for both hire and sale. Radian H.A. Ltd also has a truly unique market position as we never
+            undertake any contract work, which we would consider to be direct competition with on-site contractors, many
+            of whom are already our customers.</p>
+        <br><br>
+    </div>
+
+    <div class="slideshow-container">
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 6</div>
+            <img src="./images/slide-show/radian-img1.jpg" alt='Radian' style="width:100%">
+            <div class="text">Caption One</div>
         </div>
-
-        <div class="slideshow-container">
-            <div class="mySlides fade">
-                <div class="numbertext">1 / 6</div>
-                <img src="./images/slide-show/radian-img1.jpg" style="width:100%">
-                <div class="text">Caption One</div>
-            </div>
-            <div class="mySlides fade">
-                <div class="numbertext">2 / 6</div>
-                <img src="./images/slide-show/radian-img2.jpg" style="width:100%">
-                <div class="text">Caption Two</div>
-            </div>
-            <div class="mySlides fade">
-                <div class="numbertext">3 / 6</div>
-                <img src="./images/slide-show/radian-img3.jpg" style="width:100%">
-                <div class="text">Caption Three</div>
-            </div>
-            <div class="mySlides fade">
-                <div class="numbertext">4 / 6</div>
-                <img src="./images/slide-show/radian-img4.jpg" style="width:100%">
-                <div class="text">Caption Four</div>
-            </div>
-            <div class="mySlides fade">
-                <div class="numbertext">5 / 6</div>
-                <img src="./images/slide-show/radian-img5.jpg" style="width:100%">
-                <div class="text">Caption Five</div>
-            </div>
-            <div class="mySlides fade">
-                <div class="numbertext">6 / 6</div>
-                <img src="./images/slide-show/radian-img6.jpg" style="width:100%">
-                <div class="text">Caption Six</div>
-                <br>
-            </div>
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 6</div>
+            <img src="./images/slide-show/radian-img2.jpg" alt='Radian' style="width:100%">
+            <div class="text">Caption Two</div>
+        </div>
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 6</div>
+            <img src="./images/slide-show/radian-img3.jpg" alt='Radian' style="width:100%">
+            <div class="text">Caption Three</div>
+        </div>
+        <div class="mySlides fade">
+            <div class="numbertext">4 / 6</div>
+            <img src="./images/slide-show/radian-img4.jpg" alt='Radian' style="width:100%">
+            <div class="text">Caption Four</div>
+        </div>
+        <div class="mySlides fade">
+            <div class="numbertext">5 / 6</div>
+            <img src="./images/slide-show/radian-img5.jpg" alt='Radian' style="width:100%">
+            <div class="text">Caption Five</div>
+        </div>
+        <div class="mySlides fade">
+            <div class="numbertext">6 / 6</div>
+            <img src="./images/slide-show/radian-img6.jpg" alt='Radian' style="width:100%">
+            <div class="text">Caption Six</div>
             <br>
-            <div style="text-align: center">
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-            </div>
+        </div>
+        <br>
+        <div style="text-align: center">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
         </div>
     </div>
+</div>
 </body>
 <footer>
     <script type="text/javascript" src="./js/slideshow.js"></script>
