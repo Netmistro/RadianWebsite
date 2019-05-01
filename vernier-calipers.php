3cfe83c35b55cd324e7aabef96611db922615edb
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2019-05-01 13:19:52 -0400

    Vernier Callipers Finalized

--- vernier-calipers.php
+++ vernier-calipers.php
@@ -17,13 +17,18 @@ include 'header.php';
     <meta name="RADIAN H.A. Limited" content="Everything Scaffolding, Sale & Rental of Materials, Tools & Training">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Vernier Callipers</title>
-    <link rel="stylesheet" href="css/catalogue.css">
+    <link rel="stylesheet" href="./css/catalogue.css">
 </head>
 
 <body>
 <div class="ppe-page">
     <br>
-    <h1>Vernier Callipers</h1>
+    <h1>Vernier Callipers</h1><br>
+    <p> Compared to dial calipers, digital calipers provide measurements that are faster, easier, and more
+        error-free, but they are more fragile mechanically and electronically, and do not resist coolant well.
+        Vernier calipers are more durable than digital calipers because they do not have internal moving parts,
+        but they can be more difficult to read. Digital calipers are commonly used in fields such as metalworking,
+        mechanical engineering, machining, carpentry, and medicine.</p>
     <br>
 
     <!-- Faithfull Vernier Calliper - 150mm -->
@@ -46,8 +51,8 @@ include 'header.php';
                 protected from direct sunlight and high temperatures.
             </p>
             <br>
-            <h3>Features</h3>
-            <table align="left">
+            <table class="product-specs">
+                <th>Product Specifications:</th>
                 <tr>
                     <td>Measuring Range</td>
                     <td>0-150mm/0-6in, 0-200mm/0-8in, 0-300mm/0-12in</td>
@@ -88,6 +93,105 @@ include 'header.php';
                 <tr>
                     <td>Influence of Humidity</td>
                     <td>Not important under 80% of R.H.</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <br>
+
+    <!-- Mitutoyo Digital Vernier Calliper - 150mm -->
+    <div class="ppe-container">
+        <div class="ppe-image">
+            <img src="./images/other-products/vernier/HTC-0002.png" alt="Vernier Callipers" width="300"><br>
+        </div>
+        <br>
+        <div class="ppe-details">
+            <b><span class="product-code">Product Code:</span> HTC-0002</b><br>
+            <span class="tag-label">Description: </span><span>Mitutoyo Digital Vernier Calliper - 150mm (6")</span><br>
+            <span class="tag-label">Manufacturer: </span><span>Mitutoyo</span><br>
+            <p>
+                The Mitutoyo digital caliper has a 0 to 6" (0 to 150mm) measuring range with 0.0005" (0.01mm)
+                increments. Its digital readout LCD shows inch or metric units for versatility, and allows either
+                absolute or incremental measurement readings. The electronic caliper measures inside, outside, depth,
+                and step values for versatility and includes a protective case and SR44 silver oxide battery.
+            </p>
+            <br>
+            <table class="product-specs">
+                <th>Product Specifications:</th>
+                <tr>
+                    <td>Resolution</td>
+                    <td>0.01mm</td>
+                </tr>
+                <tr>
+                    <td>Repeatability</td>
+                    <td>0.01mm</td>
+                </tr>
+                <tr>
+                    <td>Power Supply</td>
+                    <td>Button type silver oxide SR44</td>
+                </tr>
+                <tr>
+                    <td>Battery Life</td>
+                    <td>Approx. 3.5 Years</td>
+                </tr>
+                <tr>
+                    <td>Max. Speed Response</td>
+                    <td>Unlimited</td>
+                </tr>
+                <tr>
+                    <td>Other Features</td>
+                    <td>Absolute/Incremental Measurements, Low Battery alarm & HOLD Function</td>
+                </tr>
+                <tr>
+                    <td>Range</td>
+                    <td>0-150mm / 0-6"</td>
+                </tr>
+                <tr>
+                    <td>Mass</td>
+                    <td>168g</td>
+                </tr>
+                <tr>
+                    <td>Accuracy</td>
+                    <td>+/- 0.02mm</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <br>
+
+    <!-- Moore & Wright Digital Vernier Calliper - 150mm -->
+    <div class="ppe-container">
+        <div class="ppe-image">
+            <img src="./images/other-products/vernier/HTC-0001.png" alt="Vernier Callipers" width="300"><br>
+        </div>
+        <br>
+        <div class="ppe-details">
+            <b><span class="product-code">Product Code:</span> HTC-0001</b><br>
+            <span class="tag-label">Description: </span><span>Moore & Wright Digital Vernier Calliper - 150mm (6")</span><br>
+            <span class="tag-label">Manufacturer: </span><span>Moore & Wright</span><br>
+            <p>
+                The M & W digital caliper has a 0 to 6" (0 to 150mm) measuring range. Its digital readout LCD shows inch
+                or metric units for versatility, and allows either absolute or incremental measurement readings. The
+                electronic caliper measures inside, outside, depth, and step values for versatility.
+            </p>
+            <br>
+            <table class="product-specs">
+                <th>Product Specifications:</th>
+                <tr>
+                    <td>Resolution</td>
+                    <td>0.02mm</td>
+                </tr>
+                <tr>
+                    <td>Range</td>
+                    <td>0 - 150mm/0-6"</td>
+                </tr>
+                <tr>
+                    <td>Resolution</td>
+                    <td>0.02mm</td>
+                </tr>
+                <tr>
+                    <td>Mass</td>
+                    <td>0.6504 lbs.</td>
                 </tr>
             </table>
         </div>
