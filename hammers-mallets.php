4e1d82f1cc9376ff5c1b9d4c576b6eaa38684368
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2019-05-02 16:04:43 -0400

    Hammers Added
    Multiple hammers and mallets added

--- hammers-mallets.php
+++ hammers-mallets.php
@@ -26,14 +26,14 @@ include('header.php');
         of the items that we carry and we have <b>a lot more</b> items. Please feel free to call us or come in and see
         what we have.</p><br><br>
 
-    <!-- Thor Copper Mallet 925g Plastic Handle -->
+    <!-- Thor Copper Mallet 625g Plastic Handle -->
     <div class="tools-container">
         <div class="tools-image">
             <img src="images/tools/hth-0004.png" alt="Spanner" width="300">
         </div>
         <div class="tools-details">
             <b><span class="product-code">Product Code:</span> HTH-0004</b><br>
-            <span class="tag-label">Description: </span><span>THOR Copper Mallet 925g, Plastic Handle</span><br>
+            <span class="tag-label">Description: </span><span>THOR Copper Mallet 625g, Plastic Handle</span><br>
             <span class="tag-label">Manufacturer: </span><span>THOR Hammer</span><br>
             <p>
                 Ideal for positive blows on highly finished or closely ground machined parts throughout the Aircraft,
@@ -42,27 +42,27 @@ include('header.php');
             </p>
             <br>
             <h3>Product Specifications</h3>
-            <ol>
+            <ul>
                 <li>Manufactured from 99.9% Pure Electrolytic Copper rod</li>
                 <li>Head Diameter - 38mm 1.1/2in</li>
                 <li>Approx. Length - 310mm 12.1/4in</li>
                 <li>Head Length - 89mm 3.1/2in</li>
                 <li>Plastic handle</li>
                 <li>Striking Material - Copper</li>
-                <li>Weight 930g</li>
-            </ol>
+                <li>Weight 930g, 1.5lbs.</li>
+            </ul>
         </div>
     </div>
     <br>
 
-    <!-- Thor Copper Mallet 925g Wooden Handle -->
+    <!-- Thor Copper Mallet 625g Wooden Handle -->
     <div class="tools-container">
         <div class="tools-image">
             <img src="images/tools/hth-0005.png" alt="Spanner" width="300">
         </div>
         <div class="tools-details">
             <b><span class="product-code">Product Code:</span> HTH-0005</b><br>
-            <span class="tag-label">Description: </span><span>THOR Copper Mallet 925g, Wooden Handle</span><br>
+            <span class="tag-label">Description: </span><span>THOR Copper Mallet 625g, Wooden Handle</span><br>
             <span class="tag-label">Manufacturer: </span><span>THOR Hammer</span><br>
             <p>
                 Ideal for positive blows on highly finished or closely ground machined parts throughout the Aircraft,
@@ -71,19 +71,209 @@ include('header.php');
             </p>
             <br>
             <h3>Product Specifications</h3>
-            <ol>
+            <ul>
                 <li>Manufactured from 99.9% Pure Electrolytic Copper rod</li>
-                <li>Head Diameter - 38mm 1.1/2in</li>
-                <li>Approx. Length - 310mm 12.1/4in</li>
-                <li>Head Length - 89mm 3.1/2in</li>
-                <li>Plastic handle</li>
                 <li>Striking Material - Copper</li>
-                <li>Weight 930g</li>
-            </ol>
+                <li>Round Head Diameter - 32mm 1.1/4in</li>
+                <li>Round Head Length - 82mm 3.1/4in</li>
+                <li>Approximate Weight - 970g, 1.5lbs.</li>
+                <li>Approximate Length - 280mm 11in</li>
+            </ul>
         </div>
     </div>
     <br>
 
+    <!-- Thor Copper Mallet 900g Plastic Handle -->
+    <div class="tools-container">
+        <div class="tools-image">
+            <img src="images/tools/hth-0004.png" alt="Spanner" width="300">
+        </div>
+        <div class="tools-details">
+            <b><span class="product-code">Product Code:</span> HTH-0010</b><br>
+            <span class="tag-label">Description: </span><span>THOR Copper Mallet 900g, Plastic Handle</span><br>
+            <span class="tag-label">Manufacturer: </span><span>THOR Hammer</span><br>
+            <p>
+                Ideal for positive blows on highly finished or closely ground machined parts throughout the Aircraft,
+                Automotive or Shipping Industries. Also used by tool and gauge makers and for bearing assembly in heavy
+                industrial applications such as Offshore, Earthmoving equipment, Shipping, locomotive assembly etc.
+            </p>
+            <br>
+            <h3>Product Specifications</h3>
+            <ul>
+                <li>Manufactured from 99.9% Pure Electrolytic Copper rod</li>
+                <li>Striking Material - Copper</li>
+                <li>Round Head Diameter - 38mm 1.1/2in</li>
+                <li>Round Head Length - 89mm 3.1/2in</li>
+                <li>Approximate Weight - 950g, 2.0lbs.</li>
+                <li>Approximate Length - 310mm 12.1/4in</li>
+            </ul>
+        </div>
+    </div>
+    <br>
+
+    <!-- Thor Brass Mallet 930g Plastic Handle -->
+    <div class="tools-container">
+        <div class="tools-image">
+            <img src="images/tools/hth-0011.png" alt="Spanner" width="300">
+        </div>
+        <div class="tools-details">
+            <b><span class="product-code">Product Code:</span> HTH-0011</b><br>
+            <span class="tag-label">Description: </span><span>THOR Brass Mallet 930g, Plastic Handle</span><br>
+            <span class="tag-label">Manufacturer: </span><span>THOR Hammer</span><br>
+            <p>
+                Ideal for positive blows on highly finished or closely ground machined parts throughout the Aircraft,
+                Automotive or Shipping Industries. Also used by tool and gauge makers and for bearing assembly in heavy
+                industrial applications such as Offshore, Earthmoving equipment, Shipping, locomotive assembly etc.
+            </p>
+            <br>
+            <h3>Product Specifications</h3>
+            <ul>
+                <li>Striking Material - Brass</li>
+                <li>Round Head Diameter - 38mm 1.1/2in</li>
+                <li>Round Head Length - 89mm 3.1/2in</li>
+                <li>Approximate Weight - 930g, 2.0lbs.</li>
+                <li>Approximate Length - 310mm 12.1/4in</li>
+                <li>Spark Resistant</li>
+            </ul>
+        </div>
+    </div>
+    <br>
+
+    <!-- Thor Solid Square Brass Mallet 4lbs Wooden Handle -->
+    <div class="tools-container">
+        <div class="tools-image">
+            <img src="images/tools/hth-0012.png" alt="Spanner" width="300">
+        </div>
+        <div class="tools-details">
+            <b><span class="product-code">Product Code:</span> HTH-0012</b><br>
+            <span class="tag-label">Description: </span><span>THOR Solid Brass Square Mallet 4lb., Wooden Handle</span><br>
+            <span class="tag-label">Manufacturer: </span><span>THOR Hammer</span><br>
+            <p>
+                Ideal for positive blows on highly finished or closely ground machined parts throughout the Aircraft,
+                Automotive or Shipping Industries. Also used by tool and gauge makers and for bearing assembly in heavy
+                industrial applications such as earthmoving equipment, shipping, locomotive assembly etc.
+            </p>
+            <br>
+            <h3>Product Specifications</h3>
+            <ul>
+                <li>Manufactured from extruded Brass rod</li>
+                <li>Striking Material - Brass</li>
+                <li>Approximate Length - 405mm 16in</li>
+                <li>Square Face Size - 44mm 1.3/4in</li>
+                <li>Square Head Length - 114mm 4.1/2in</li>
+                <li>Approximate Weight - 1890g</li>
+                <li>Spark Resistant</li>
+            </ul>
+        </div>
+    </div>
+    <br>
+
+    <!-- Thor Solid Round Brass Mallet 670g Plastic Handle -->
+    <div class="tools-container">
+        <div class="tools-image">
+            <img src="images/tools/hth-0013.png" alt="Spanner" width="300">
+        </div>
+        <div class="tools-details">
+            <b><span class="product-code">Product Code:</span> HTH-0013</b><br>
+            <span class="tag-label">Description: </span><span>THOR Solid Round Brass Mallet 670g, Plastic Handle</span><br>
+            <span class="tag-label">Manufacturer: </span><span>THOR Hammer</span><br>
+            <p>
+                Ideal for positive blows on highly finished or closely ground machined parts throughout the Aircraft,
+                Automotive or Shipping Industries. Also used by tool and gauge makers and for bearing assembly in heavy
+                industrial applications such as earthmoving equipment, shipping, locomotive assembly etc.
+            </p>
+            <br>
+            <h3>Product Specifications</h3>
+            <ul>
+                <li>Manufactured from extruded Brass rod</li>
+                <li>Striking Material - Brass</li>
+                <li>Round Head Diameter - 32mm 1.1/4in</li>
+                <li>Round Head Length - 82mm 3.1/4in</li>
+                <li>Approximate Length - 300mm 11.3/4in</li>
+                <li>Approximate Weight - 670g, 1.5lbs.</li>
+                <li>Spark Resistant</li>
+            </ul>
+        </div>
+    </div>
+    <br>
+
+    <!-- Silverline Claw Hammer with Hickory handle -->
+    <div class="tools-container">
+        <div class="tools-image">
+            <img src="images/tools/hth-0002.png" alt="Spanner" width="300">
+        </div>
+        <div class="tools-details">
+            <b><span class="product-code">Product Code:</span> HTH-0002</b><br>
+            <span class="tag-label">Description: </span><span>Silverline Claw Hammer with Hickory handle</span><br>
+            <span class="tag-label">Manufacturer: </span><span>Silverline</span><br>
+            <p>
+                Ideal for carpentry or around the house work.
+            </p>
+            <br>
+            <h3>Product Specifications</h3>
+            <ul>
+                <li>Forged steel head and hickory shaft</li>
+                <li>Polished and lacquered head and claw</li>
+                <li>Varnished handle for water and dirt resistance</li>
+                <li>For fixing and removing nails</li>
+                <li>Dimensions - 13.2" x 1.2" x 5.1"</li>
+                <li>Mass - 1.0lbs.</li>
+            </ul>
+        </div>
+    </div>
+    <br>
+
+    <!-- EGA Master Claw Hammer with Hickory handle -->
+    <div class="tools-container">
+        <div class="tools-image">
+            <img src="images/tools/hth-0008.png" alt="Spanner" width="300">
+        </div>
+        <div class="tools-details">
+            <b><span class="product-code">Product Code:</span> HTH-0008</b><br>
+            <span class="tag-label">Description: </span><span>EGA Master Brass Claw Hammer with Plastic Handle</span><br>
+            <span class="tag-label">Manufacturer: </span><span>EGA Master</span><br>
+            <p>
+                Ideal for removing nails from scaffold boards offshore, carpentry or around the house work.
+            </p>
+            <br>
+            <h3>Product Specifications</h3>
+            <ul>
+                <li>Rubber Grip</li>
+                <li>Spark Resistant</li>
+                <li>Plastic Handle</li>
+                <li>Mass - 1.1 lbs.</li>
+            </ul>
+        </div>
+    </div>
+    <br>
+
+    <!-- NUPLA Solid Brass Mallet, Fibre Glass Handle 2.5lbs. -->
+    <div class="tools-container">
+        <div class="tools-image">
+            <img src="images/tools/hth-0007.png" alt="Spanner" width="300">
+        </div>
+        <div class="tools-details">
+            <b><span class="product-code">Product Code:</span> HTH-0008</b><br>
+            <span class="tag-label">Description: </span><span>NUPLA Solid Brass Master Brass Claw Hammer with Fibre Glass Handle</span><br>
+            <span class="tag-label">Manufacturer: </span><span>NUPLA</span><br>
+            <p>
+                Ideal for scaffolding using Cuplock materials & Offshore use, where non sparking tools are required.
+            </p>
+            <br>
+            <h3>Product Specifications</h3>
+            <ul>
+                <li>Rubber Grip</li>
+                <li>Fibre Glass Handle</li>
+                <li>Spark Resistant</li>
+                <li>Solid Brass</li>
+                <li>Mass - 2.5 lbs.</li>
+                <li>Handle Length - 12"</li>
+                <li>Heavy non-spark striking face with soft impact</li>
+                <li>Malleable brass material will not fracture or create shards when striking hard surfaces</li>
+            </ul>
+        </div>
+    </div>
+    <br>
 </div>
 </body>
 <footer>
