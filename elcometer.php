cc6a795375d09f479cc1c664fbfbe5608638bbec
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2019-08-09 12:01:07 -0400

    Update eleometer page with additional item - Elcometer 500

--- elcometer.php
+++ elcometer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * User: Arnold Bradshaw
  * Date: 4/26/19
@@ -27,7 +28,7 @@ include 'header.php';
             <!-- Elcometer Paint Inspection Kit 1 - Basic -->
             <div class="ppe-container">
                 <div class="ppe-image">
-                    <img src="./images/inspection/elcometer-paint-inspection-kit-1.png" alt="Painting Kit 1"
+                    <img src="./images/inspection/elcometer-paint-inspection-kit-1.png" alt="Paint Inspection Kit"
                         width="300"><br>
                 </div>
                 <br>
@@ -49,7 +50,7 @@ include 'header.php';
                         coating is, or has been, applied correctly and is fit for purpose, removing the need for
                         guesswork
                     </p><br>
-                    <h3>Kit Contents:-</h3>
+                    <h3>Kit Contents: -</h3>
                     <ol>
                         <li>Testex Replica Tape - Coarse, 20 - 64μm (0.5 - 2.5mils)</li>
                         <li>Replica Tape - X-Coarse, 38 - 115μm (1.5 - 4.5mils)</li>
@@ -62,6 +63,71 @@ include 'header.php';
                 </div>
             </div>
             <br>
+
+            <!-- Elcometer Concrete Inspection Kit -->
+            <div class="ppe-container">
+                <div class="ppe-image">
+                    <img src="./images/inspection/elcometer-paint-inspection-kit-2.png" alt="Concrete Inspection Kit"
+                        width="300"><br>
+                </div>
+                <br>
+                <div class="ppe-details">
+                    <b><span class="product-code">Product Code:</span> PIK-0002</b><br>
+                    <span class="tag-label">Description: </span><span>Elcometer Protective Coating Inspection
+                        Kit</span><br>
+                    <span class="tag-label">Manufacturer: </span><span>Elcometer</span>
+                    <br>
+                    <p>
+                        The Elcometer 500 Coating Thickness Gauge accurately measures the thickness of coatings on
+                        concrete and other similar substrates* - non destructively.
+                    </p><br>
+                    <p>
+                        Fast and accurate, the Elcometer 500 Coating Thickness Gauge takes repeatable and reproducible
+                        dry film thickness measurements of coatings on concrete up to 10mm‡ thick – without damaging the
+                        coating.
+                    </p>
+                    <p>
+                        Taking over 60 readings per minute in standard mode and 140+ readings per minute in scan mode;
+                        the Elcometer 500 Coating Thickness Gauge allows users to inspect more coatings in less time.
+                        With a choice of intelligent, ergonomic probes, designed for continuous use and field
+                        replaceable probe tips you can inspect all day, everyday.
+                    </p>
+                    <p>
+                        The built in signal strength indicator on the Elcometer 500 Coatings on Concrete Gauge prevents
+                        false or incorrect readings, as the gauge only displays the coating thickness measurement if the
+                        signal strength indicator goes green.
+                    </p>
+                    <p>
+                        Robust, ergonomic and sealed against dirt and water, equivalent to a rating of IP54, the
+                        Elcometer 500 Concrete Coating Thickness Gauge has been designed to work in harsh environments.
+                    </p><br>
+                    <h3>Kit Contents: -</h3>
+                    <ol>
+                        <li>Compatible with Elcometer Software & Elcometer Mobile App</li>
+                        <li>Fast - Measuring over 60 readings per minute in standard mode and over 140 readings per
+                            minute in scan mode</li>
+                        <li>Reliable - The Elcometer 500 will only display the coating thickness reading if the signal
+                            strength indicator turns green, preventing false or incorrect readings</li>
+                        <li>Accuracy - Elcometer 500 has a measurement accuracy of ±2%</li>
+                        <li>Operating Temperature - -10 to 50°C (14 to 122°F)</li>
+                        <li>Power Supply - 2 x AA batteries (rechargable batteries can be used)</li>
+                        <li>Battery Life - Alkaline: Approximately 15 hours Lithium: Approximately 28 hours </li>
+                        <li>Weight - 161g (5.68oz) including batteries, without transducer</li>
+                        <li>Dimensions - 141 x 73 x 37mm (5.55 x 2.87 x 1.46”) without transducer</li>
+                        <li>Probe Range C1 - Range1: 150 - 2,500μm (6 - 98mils) Accuracy2: ±2% or ±10μm (±2% or ±0.4mil)
+                        </li>
+                        <li>Probe Resolution C1 - Resolution: Low: 10μm, 0.01mm, 1mil or 0.001” High: 1μm, 0.001mm,
+                            0.1mil or 0.0001”</li>
+                        <li>Probe Range C2 - Range1: 750 - 9,000μm (30 - 355mils) Accuracy2: ±2% or ±10μm (±2% or
+                            ±0.4mil)</li>
+                        <li>Probe Resolution C2 - Resolution: Low: 10μm, 0.01mm, 1mil or 0.001” High: 1μm, 0.001mm,
+                            0.1mil or 0.0001”
+                        </li>
+                    </ol>
+                </div>
+            </div>
+
+
         </div>
     </body>
     <footer>
