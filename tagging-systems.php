30363c52c29a2121cb0129934d3362cc4cac932c
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2020-02-05 10:31:03 -0400

    Added RED Scafftag to Tagging Systems

--- tagging-systems.php
+++ tagging-systems.php
@@ -10,6 +10,7 @@ include('header.php');
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="RADIAN H.A. Limited" content="Everything Scaffolding, Sale & Rental of Materials, Tools & Training">
@@ -17,85 +18,106 @@ include('header.php');
     <title>Tagging Systems</title>
     <link rel="stylesheet" href="css/catalogue.css">
 </head>
+
 <body>
-<br>
-<!-- Green Scafftags -->
-<div class="tagging-system">
-    <h1>Tagging Systems</h1><br>
-    <div class="tagging-system-container">
-        <div class="tagging-image">
-            <img src="images/tagsystems/scafftag-green.png" alt="Scafftag-Green" width="300">
-        </div>
-        <div class="tagging-details">
-            <b><span class="product-code">Product Code:</span> STI-005</b><br>
-            <span class="tag-label">Description: </span><span>Scafftag - Green Inspection tags are internationally recognized as the
-        safety standard for tagging and controlling scaffold inspection.</span><br>
-            <span class="tag-label">Manufacturer: </span><span>SCAFFTAG</span><br>
-        </div>
-    </div>
     <br>
-    <!-- Coloured Scafftags -->
-    <div class="tagging-system-container">
-        <div class="tagging-image">
-            <img src="images/tagsystems/scafftag-color-coded-inserts.png" alt="Scafftag-Color" width="300">
+    <!-- Major Headers -->
+    <div class="tagging-system">
+        <h1>Tagging Systems</h1><br>
+        <!-- Green Scafftags -->
+        <div class="tagging-system-container">
+            <div class="tagging-image">
+                <img src="images/tagsystems/scafftag-green.png" alt="Scafftag-Green" width="300">
+            </div>
+            <div class="tagging-details">
+                <b><span class="product-code">Product Code:</span> STI-005</b><br>
+                <span class="tag-label">Description: </span><span>Scafftag - Green Inspection tags are internationally
+                    recognized as the
+                    safety standard for tagging and controlling scaffold inspection.</span><br>
+                <span class="tag-label">Manufacturer: </span><span>SCAFFTAG</span><br>
+            </div>
         </div>
-        <div class="tagging-details">
-            <b><span class="product-code">Product Code:</span> STI-007-011</b><br>
-            <span class="tag-label">Description: </span><span>Scafftag - Coloured Inspection Tags available in Grey,
-                Magenta, Orange, Blue & Brown for all the different classes of Scaffolding.</span><br>
-            <span class="tag-label">Manufacturer: </span><span>SCAFFTAG</span><br>
-            <p>Grey - Very Light Duty</p>
-            <p>Magenta - Light Duty</p>
-            <p>Orange - General Purpose</p>
-            <p>Blue - Heavy Duty</p>
-            <p>Brown - Special Purpose</p>
-        </div>
-    </div>
-    <br>
-    <!-- Yellow Scafftags -->
-    <div class="tagging-system-container">
-        <div class="tagging-image">
-            <img src="images/tagsystems/scafftag-yellow.png" alt="Scafftag-Yellow" width="300">
-        </div>
-        <div class="tagging-details">
-            <b><span class="product-code">Product Code:</span> STI-004</b><br>
-            <span class="tag-label">Description: </span><span>Scafftag - (Incomplete Scaffold) Yellow Inspection tags
-                for tagging and controlling scaffold inspection.</span><br>
-            <span class="tag-label">Manufacturer: </span><span>SCAFFTAG</span><br>
-        </div>
-    </div>
-    <br>
+        <br>
 
-    <!--    Scafftag Holders -->
-    <div class="tagging-system-container">
-        <div class="tagging-image">
-            <img src="images/tagsystems/scafftag-holder.png" alt="Scafftag-Holder" width="300">
+        <!-- RED Scafftags -->
+        <div class="tagging-system-container">
+            <div class="tagging-image">
+                <img src="images/tagsystems/scafftag-red.png" alt="Scafftag-Green" width="300">
+            </div>
+            <div class="tagging-details">
+                <b><span class="product-code">Product Code:</span> STI-029</b><br>
+                <span class="tag-label">Description: </span><span>Scafftag - RED Scaffold Under Construction/Prohibition
+                    Notice</span><br>
+                <span class="tag-label">Manufacturer: </span><span>SCAFFTAG</span><br>
+            </div>
         </div>
-        <div class="tagging-details">
-            <b><span class="product-code">Product Code:</span> STH-002</b><br>
-            <span class="tag-label">Description: </span><span>Scafftag - Scaffold Tag Holder, 11-3/4" Height, 3-1/2"
-                Width. Front Legend "Danger, Do Not Use Scaffold" </span><br>
-            <span class="tag-label">Manufacturer: </span><span>SCAFFTAG</span><br>
-        </div>
-    </div>
-    <br>
+        <br>
 
-    <!--    Scafftag Holder RED -->
-    <div class="tagging-system-container">
-        <div class="tagging-image">
-            <img src="images/tagsystems/scafftag-do-not-use.png" alt="Scafftag-Holder" width="300">
+        <!-- Coloured Scafftags -->
+        <div class="tagging-system-container">
+            <div class="tagging-image">
+                <img src="images/tagsystems/scafftag-color-coded-inserts.png" alt="Scafftag-Color" width="300">
+            </div>
+            <div class="tagging-details">
+                <b><span class="product-code">Product Code:</span> STI-007-011</b><br>
+                <span class="tag-label">Description: </span><span>Scafftag - Coloured Inspection Tags available in Grey,
+                    Magenta, Orange, Blue & Brown for all the different classes of Scaffolding.</span><br>
+                <span class="tag-label">Manufacturer: </span><span>SCAFFTAG</span><br>
+                <p>Grey - Very Light Duty</p>
+                <p>Magenta - Light Duty</p>
+                <p>Orange - General Purpose</p>
+                <p>Blue - Heavy Duty</p>
+                <p>Brown - Special Purpose</p>
+            </div>
         </div>
-        <div class="tagging-details">
-            <b><span class="product-code">Product Code:</span> STH-008</b><br>
-            <span class="tag-label">Description: </span><span>BRADY - Scaffold Tag Holder (All Red Color) </span><br>
-            <span class="tag-label">Manufacturer: </span><span>BRADY</span><br>
+        <br>
+        <!-- Yellow Scafftags -->
+        <div class="tagging-system-container">
+            <div class="tagging-image">
+                <img src="images/tagsystems/scafftag-yellow.png" alt="Scafftag-Yellow" width="300">
+            </div>
+            <div class="tagging-details">
+                <b><span class="product-code">Product Code:</span> STI-004</b><br>
+                <span class="tag-label">Description: </span><span>Scafftag - (Incomplete Scaffold) Yellow Inspection
+                    tags
+                    for tagging and controlling scaffold inspection.</span><br>
+                <span class="tag-label">Manufacturer: </span><span>SCAFFTAG</span><br>
+            </div>
         </div>
-    </div>
-    <br>
+        <br>
 
-</div>
+        <!--    Scafftag Holders -->
+        <div class="tagging-system-container">
+            <div class="tagging-image">
+                <img src="images/tagsystems/scafftag-holder.png" alt="Scafftag-Holder" width="300">
+            </div>
+            <div class="tagging-details">
+                <b><span class="product-code">Product Code:</span> STH-002</b><br>
+                <span class="tag-label">Description: </span><span>Scafftag - Scaffold Tag Holder, 11-3/4" Height, 3-1/2"
+                    Width. Front Legend "Danger, Do Not Use Scaffold" </span><br>
+                <span class="tag-label">Manufacturer: </span><span>SCAFFTAG</span><br>
+            </div>
+        </div>
+        <br>
+
+        <!--    Scafftag Holder RED -->
+        <div class="tagging-system-container">
+            <div class="tagging-image">
+                <img src="images/tagsystems/scafftag-do-not-use.png" alt="Scafftag-Holder" width="300">
+            </div>
+            <div class="tagging-details">
+                <b><span class="product-code">Product Code:</span> STH-008</b><br>
+                <span class="tag-label">Description: </span><span>BRADY - Scaffold Tag Holder (All Red Color)
+                </span><br>
+                <span class="tag-label">Manufacturer: </span><span>BRADY</span><br>
+            </div>
+        </div>
+        <br>
+
+    </div>
 </body>
 <footer>
     <?php include('footer.php'); ?>
 </footer>
+
 </html>
