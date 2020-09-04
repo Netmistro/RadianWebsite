e01446c01de272d7dff2037f47961be0dbf1a7fd
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2020-09-03 22:36:16 -0400

    Remove Brackets from Telephone Nos.

--- banner.php
+++ banner.php
@@ -18,12 +18,12 @@
 <body>
     <div class="banner">
         <h4>IMPORTANT Information</h4>
-        <p>Please be advised that our Primary Office line (868) 658-0293 is temporarily out of service. However
+        <p>Please be advised that our Primary Office line 868-658-0293 is temporarily out of service. However
             we can be reached on the following additional numbers listed below:-</p>
         <ol>
-            <li>TSTT - (868) 658-8294</li>
-            <li>FLOW - (868) 223-8823/4</li>
-            <li>DIGICEL - (868) 387-0293</li>
+            <li>TSTT - 868-658-8294</li>
+            <li>FLOW - 868-223-8823/4</li>
+            <li>DIGICEL - 868-387-0293</li>
         </ol>
     </div>
 </body>
