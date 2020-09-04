f1bd4518e8ce15f2b3e35cde049a1f729c35e891
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2020-09-03 22:39:48 -0400

    Update Banner File

--- banner.php
+++ banner.php
@@ -1,33 +1,32 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Arnold Bradshaw
  * Date: 04/30/19
  * Time: 11:00 PM
  */
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <link rel="stylesheet" href="./css/banner.css">
-</head>
+    <head>
+        <link rel="stylesheet" href="./css/banner.css">
+    </head>
 
-<body>
-    <div class="banner">
-        <h4>IMPORTANT Information</h4>
-        <p>Please be advised that our Primary Office line 868-658-0293 is temporarily out of service. However
-            we can be reached on the following additional numbers listed below:-</p>
-        <ol>
-            <li>TSTT - 868-658-8294</li>
-            <li>FLOW - 868-223-8823/4</li>
-            <li>DIGICEL - 868-387-0293</li>
-        </ol>
-    </div>
-</body>
-<footer>
-</footer>
+    <body>
+        <div class="banner">
+            <h4>IMPORTANT Information</h4>
+            <p>Please be advised that our Primary Office line 868-658-0293 is temporarily out of service. However
+                we can be reached on the following additional numbers listed below:-</p>
+            <ol>
+                <li>TSTT - 868-658-8294</li>
+                <li>FLOW - 868-223-8823/4</li>
+                <li>DIGICEL - 868-387-0293</li>
+            </ol>
+        </div>
+    </body>
+    <footer>
+    </footer>
 
 </html>
