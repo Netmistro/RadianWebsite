a6ae93a6a78f6623fc7dc41097244bd02aa1465d
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2020-09-22 21:59:14 -0400

    Remove PHPMailer

--- test.php
+++ test.php
@@ -1,67 +1,51 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: Brad
- * Date: 10/8/2018
- * Time: 11:03 AM
+ * Created by VSCODE.
+ * User: Arnold Bradshaw
+ * Date: 2020-10-22
+ * Time: 09:19
  */
-include 'header.php';
-?>
 
+include('header.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Contact-Us</title>
+        <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/main-style.css">
-        <link rel="stylesheet" href="css/test.css">
+        <link rel="stylesheet" href="./css/test.css">
     </head>
 
+    <!--Changing the number in the column_# class changes the number of columns-->
+
     <body>
-        <div class="main-page">
-            <iframe allowfullscreen="" allow="fullscreen" style="border:none;width:100%;height:800px;"
-                src="//e.issuu.com/embed.html?d=the_scaffolder___s_choice_workwear_&amp;hideIssuuLogo=true&amp;u=leachssafety"></iframe>
+        <div id="wrap">
+            <h1>Scaffmart - H-Frames</h1><br>
+            <div id="columns" class="columns_4">
+                <figure>
+                    <img src="images/scaff-mart/scaff-mart-5x3x7.png" alt="Scaffoldmart Set" width="300">
+                    <figcaption>Scaffmart 5'x 3' Set w/ 7' Bracing</figcaption>
+                    <span class="sku">SKU: SMP-0001</span>
+                    <a class="button" href="#">More</a>
+                </figure>
+            </div>
 
-            <iframe allowfullscreen="" allow="fullscreen" style="border:none;width:100%;height:800px;"
-                src="//e.issuu.com/embed.html?d=0120_mpg_issue_1&amp;hideIssuuLogo=true&amp;u=leachssafety"></iframe>
-
-            <iframe allowfullscreen="" allow="fullscreen" style="border:none;width:100%;height:800px;"
-                src="//e.issuu.com/embed.html?d=oil__gas___energy_scaffolding___hei&amp;hideIssuuLogo=true&amp;u=leachssafety"></iframe>
-
-            <style>
-            .embed-container {
-                position: relative;
-                padding-bottom: 56.25%;
-                height: 0;
-                overflow: hidden;
-                max-width: 100%;
-            }
-
-            .embed-container iframe,
-            .embed-container object,
-            .embed-container embed {
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-            }
-            </style>
-            <div class='embed-container' data-page-width='495' data-page-height='640' id='ypembedcontainer'><iframe
-                    src="https://www.yumpu.com/en/embed/view/vR3QI9xPXNFhVzat" frameborder="0" allowfullscreen="true"
-                    allowtransparency="true"></iframe></div>
-            <script src='https://players.yumpu.com/modules/embed/yp_r_iframe.js'></script>
-
-        </div>
-        <script src="" async defer></script>
+            <div id="columns" class="columns_4">
+                <figure>
+                    <img src="images/scaff-mart/scaff-mart-5x5x7.png" alt="Scaffoldmart Set" width="300">
+                    <figcaption>Scaffmart 5' X 5' Set w/ 7' Bracing</figcaption>
+                    <span class="sku">SKU: SMP-0002</span>
+                    <a class="button" href="#">More</a>
+                </figure>
+            </div>
     </body>
     <footer>
-        <?php include "footer.php"; ?>
+        <?php include('footer.php'); ?>
     </footer>
 
 </html>
